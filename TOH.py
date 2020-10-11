a56946cd75c63b2a132707adc242a0082eb03a50
Harshith B <64398730+HarshithB@users.noreply.github.com>
2020-10-11 15:48:02 +0530

    Added TOH

---
+++ TOH.py
@@ -0,0 +1,19 @@
+# https://www.facebook.com/harsith.gayle.1/posts/2784315391846205
+# Subscribed by Code House
+
+
+
+
+# Recursive Python function to solve the tower of hanoi 
+  
+def TowerOfHanoi(n , source, destination, auxiliary): 
+    if n==1: 
+        print "Move disk 1 from source",source,"to destination",destination 
+        return
+    TowerOfHanoi(n-1, source, auxiliary, destination) 
+    print "Move disk",n,"from source",source,"to destination",destination 
+    TowerOfHanoi(n-1, auxiliary, destination, source) 
+          
+# Driver code 
+n = 4
+TowerOfHanoi(n,'A','B','C')  
